13667778e08b1568002dc5c01d0f0f890cd5b1e3
Derrick Schultz <derrick.schultz@nytimes.com>
2019-01-29 11:54:01 -0500

    svg added

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,6 @@ const nav = document.getElementById("main-nav");
 
 btn.addEventListener('click', event => {
   nav.classList.toggle("hidden");
+  btn.classList.toggle("close");
   return false;
 }) 
